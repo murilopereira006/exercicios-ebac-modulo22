aa7688aa13687415f512af76bb3e7361d6da8f20
muriloPereira006 <muriloedu006@gmail.com>
2023-12-05 23:10:32 -0300

    add: 1

--- cypress/e2e/criaçãoDeConta.cy.js
+++ cypress/e2e/criaçãoDeConta.cy.js
@@ -1,7 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
+import { faker } from '@faker-js/faker';
+const email = faker.internet.email()
+
+describe('exercicio 1', () => {
+  it('fluxo de criação de conta,', () => {
     cy.visit('http://lojaebac.ebaconline.art.br/')
 
     cy.get('.icon-user-unfollow').click()
+
+    cy.get('#reg_email').type(email)
+    cy.get('#reg_password').type('text123')
+
+    cy.get(':nth-child(4) > .button').click()
+
+    cy.contains('A partir do painel de controle de sua conta, você pode ver suas compras recentes').should('be.visible')
   })
 })
